78ce8db86e701fa3afa23240810524384685bb30
Abdurrahman Gurib <abdurrahman.gurib@sdworx.com>
2024-02-23 15:29:54 +0400

    Add 3 cards report with buttons to access reports for each team

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class ReportsComponent {
 
+  constructor() { }
+
+  seeReports(team: string) {
+    // Assuming you have the PDF reports stored in a directory accessible by the application
+    let url: string;
+
+    switch (team) {
+      case 'FunAtWork':
+        url = 'path_to_fun_at_work_reports';
+        break;
+      case 'PayBackTeam':
+        url = 'path_to_payback_team_reports';
+        break;
+      case 'HRTeam':
+        url = 'path_to_hr_team_reports';
+        break;
+      default:
+        url = '';
+        break;
+    }
+
+    // Open the PDF report in a new tab
+    window.open(url, '_blank');
+  }
 }
